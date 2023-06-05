8aae0a3f5db3a3df3123414714002a827a68632e
David Freire Fernández <93536906+DFreireF@users.noreply.github.com>
2023-06-05 17:28:20 +0200

    Update tdms_scaler_reader.py

--- tdms_scaler_reader.py
+++ tdms_scaler_reader.py
@@ -31,11 +31,10 @@ def read_tdms_scaler(filename, channel=5):
     
 def kicker_times(filename, fs = 1024): # kicker_time != injection time ?
     tdms_file = TdmsFile.read(filename)
-    kicker_channel = tdms_file['SCData'][f'CHANNEL_04']
+    kicker_channel = tdms_file['SCData'][f'CHANNEL_04'][:]
     initial_timestamp = tdms_file['SCTimestamps']['TimeStamp'][0]
-    b = kicker_channel[:]
-    bjump = np.where(np.diff(b) == 1)[0]
-    kicker_times = [initial_timestamp + np.timedelta64(int((index+1)/fs*1e9),'ns') for index in bjump]
+    jump = np.where(np.diff(kicker_channel) == 1)[0]
+    kicker_times = initial_timestamp + np.timedelta64(int((jump+1)/fs*1e9),'ns')
     return kicker_times
 
 def print_tdms_info(file_path): #Of iq.tdms and iq.tdms_info
